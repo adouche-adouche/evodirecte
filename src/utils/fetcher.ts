7831469e9fdbedb48abf5f575b46dbce689fec02
Adouche <mr.froid.84@gmail.com>
2026-03-02 18:48:30 +0100

    Update fetcher.ts

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -16,7 +16,7 @@ export class Fetcher {
         private uuid?: string
     ) {
         if (this.uuid) {
-            this.userAgent = `evoDirecte/1.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 EDMOBILE v${API_VERSION}`;
+            this.userAgent = `wrapDirecte/1.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 EDMOBILE v${API_VERSION}`;
         }
     }
 
@@ -30,7 +30,7 @@ export class Fetcher {
 
     setUuid(uuid: string) {
         this.uuid = uuid;
-        this.userAgent = `evoDirecte/1.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 EDMOBILE v${API_VERSION}`;
+        this.userAgent = `wrapDirecte/1.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 EDMOBILE v${API_VERSION}`;
     }
 
     async request<T>(options: RequestOptions): Promise<EDResponse<T>> {
